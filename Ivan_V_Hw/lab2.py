4128622121a3c092e1f881629fa9b493a621a19b
ivasilofficial <97457571+ivasilofficial@users.noreply.github.com>
2022-03-14 22:12:57 +0300

    Добавил несколько переменных

--- Ivan_V_Hw/lab2.py
+++ Ivan_V_Hw/lab2.py
@@ -1,10 +1,16 @@
 import math
+
 """
 это решение второй лабороторной работы
 """
+
 def math2x():
     x_start = float(input())
     x_end = float(input())
     dx = float(input())
     e = math.exp()
-
+    a = 0
+    b = 0
+    c = 0
+    d = 0
+  
