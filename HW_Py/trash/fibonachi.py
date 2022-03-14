fd02285bf82819bf8deb89fc695e602f93edaa7e
toborix <matveybor394@gmail.com>
2022-03-14 20:29:19 +0300

    remove HW

--- HW_Py/trash/fibonachi.py
+++
@@ -1,5 +0,0 @@
-a = 0
-b = 1
-while b < 100:
-    print(b)
-    a, b = b, a + b
--- HW_Py/trash/loan.py
+++
@@ -1,8 +0,0 @@
-import math
-loan = 2e6;
-rate = (5.5/100) / 12;
-term = 2;
-month = term * 12;
-payment = math.floor((loan*rate/(1-math.pow(1+rate,(-1*month))))*100)/100;
-print(payment)
-
