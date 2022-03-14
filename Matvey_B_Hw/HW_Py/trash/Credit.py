a56f8eff2834adbcd4439dfe4270831bb011a6e3
toborix <matveybor394@gmail.com>
2022-03-14 22:37:12 +0300

    изменил файл Credit

--- Matvey_B_Hw/HW_Py/trash/Credit.py
+++ Matvey_B_Hw/HW_Py/trash/Credit.py
@@ -1,9 +1,11 @@
 import math
 
-p = float(input('Введите сумму, положенную на счет: '))
-r = float(input('Введите годовую процентную ставку: '))
-n = int(input('Введите количество лет: '))
-a = math.pow(p * (1 + r), n)
+
+
+pip = float(input('Введите сумму, положенную на счет: '))
+rir = float(input('Введите годовую процентную ставку: '))
+nir = int(input('Введите количество лет: '))
+aia = math.pow(p * (1 + r), n)
 
 print('Сумма, полученная через', n, 'лет =', a)
 mounth_paid = float(math.ceil(a/36))
